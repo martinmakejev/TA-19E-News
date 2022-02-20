f57e3b8b98007e239cf1921ca467803d8c1e0bd1
Artur Allmann <Artur.Allmann@tptlive.ee>
2022-02-20 20:12:16 +0200

    .env, schema
    created .env template,
    updated schema.prisma. news-comment constraint not working

---
+++ prisma/migrations/20220220180142_create_tables/migration.sql
@@ -0,0 +1,59 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `news` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `school_id` INTEGER NOT NULL,
+    `class_id` INTEGER NOT NULL,
+    `news_title` VARCHAR(191) NOT NULL,
+    `author_name` VARCHAR(191) NOT NULL,
+    `news_content` VARCHAR(191) NOT NULL,
+    `news_images` VARCHAR(191) NOT NULL,
+    `release_date` DATETIME(3) NOT NULL,
+    `comment_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `author` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `school` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `classes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `news` ADD CONSTRAINT `news_school_id_fkey` FOREIGN KEY (`school_id`) REFERENCES `school`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `news` ADD CONSTRAINT `news_class_id_fkey` FOREIGN KEY (`class_id`) REFERENCES `classes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment` ADD CONSTRAINT `comment_id_fkey` FOREIGN KEY (`id`) REFERENCES `news`(`comment_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
