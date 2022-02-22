ebbac430521d5c54d1da4213d9096897f938ddf3
Ingela <59916738+JoinEliza@users.noreply.github.com>
2022-02-23 00:29:05 +0200

    main page header

---
+++ resources/components/main_layout.js
@@ -0,0 +1,37 @@
+import { Layout, Menu, Button, Space, Input, Row, Col } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+
+export default function MainLayout(){
+    return(
+        <div>
+            <Layout className="layout" style={{backgroundColor:'white'}}>
+                <Header style={{ backgroundColor: 'white'}}>
+                    <Row>
+                        <Col span={6}>
+                            <div className="logo"/>
+                        </Col>
+                        <Col span={6}>
+                            <Menu>
+                            </Menu>
+                        </Col>
+                        <Col span={6}>
+                            <Button onClick={()=>{window.location.href="/login-panel"}}>Log In</Button>
+                        </Col>
+                        <Col span={6}>
+                            <Search placeholder="Otsi" style={{ width: 200 }} />
+                        </Col>
+                    </Row>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <div className="site-layout-content">
+                        <div style={{height:300}}></div>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </div>
+    )
+    
+}
