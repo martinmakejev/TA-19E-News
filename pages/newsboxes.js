5349724d360603979a47e658dabc7aa139c63829
AndreasLepsuk <60178041+AndreasLepsuk@users.noreply.github.com>
2022-03-04 14:14:33 +0200

    changes to newsbox css and js

--- pages/newsboxes.js
+++ pages/newsboxes.js
@@ -8,12 +8,50 @@ import styles from '../styles/boxes.module.css'
 export default function newsboxes() {
     return (
         <div className={styles.container}>
-                <div className={styles.mainNews}>
-                    <h2>Lorem ipsum</h2>
-                    <p>asadfdshfjsdajfhksdfjksdhfuisadjfsda</p>
-                </div>
-                <div className={styles.news}>Lorem ipsum 1</div>
-                <div className={styles.news1}>Lorem ipsum 2</div>
+            <div className={styles.mainNews}>
+                <h2>Lorem ipsum</h2>
+                <p>asadfdshfjsdajfhksdfjksdhfuisadjfsda</p>
+            </div>
+            <div className={styles.news}>
+               <h2>Lorem ipsum 1</h2>
+               <p>dsafsdsdsdfsdfasfasdfasfdasdfa</p>
+            </div>
+            <div className={styles.news1}>
+                <h2>Lorem Ipsum 2</h2>
+                <p>asfsafsfsfasdfsadf</p>
+            </div>
+            <div className={styles.news2}>
+                <h2>Lorem ipsum 3</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news3}>
+                <h2>Lorem ipsum 4</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news4}>
+                <h2>Lorem ipsum 5</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news5}>
+                <h2>Lorem ipsum 6</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news6}>
+                <h2>Lorem ipsum 7</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news7}>
+                <h2>Lorem ipsum 8</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news8}>
+                <h2>Lorem ipsum 9</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
+            <div className={styles.news9}>
+                <h2>Lorem ipsum 10</h2>
+                <p>asdfasfsdafsdfsafaas</p>
+            </div>
         </div>
     );
 }
