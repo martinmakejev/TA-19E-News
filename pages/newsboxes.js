c620a1dd8e0659381f991fe4db6ef24cef869258
AndreasLepsuk <60178041+AndreasLepsuk@users.noreply.github.com>
2022-03-08 11:44:11 +0200

    Installisin ant design ja tegin valmis newsboxes hetkel küll piltideta.

--- pages/newsboxes.js
+++ pages/newsboxes.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from '../styles/boxes.module.css'
+import { Image } from "antd";
+import "antd/dist/antd.css";
 
 //logo header
 //nav content
@@ -8,49 +10,49 @@ import styles from '../styles/boxes.module.css'
 export default function newsboxes() {
     return (
         <div className={styles.container}>
-            <div className={styles.mainNews}>
+            <div className={styles.mainNews}>'
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum</h2>
-                <p>asadfdshfjsdajfhksdfjksdhfuisadjfsda</p>
             </div>
             <div className={styles.news}>
+                <img width={200} src="../pages/placeholder.svg"/>
                <h2>Lorem ipsum 1</h2>
-               <p>dsafsdsdsdfsdfasfasdfasfdasdfa</p>
             </div>
             <div className={styles.news1}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem Ipsum 2</h2>
-                <p>asfsafsfsfasdfsadf</p>
             </div>
             <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 3</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
             <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 4</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news4}>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 5</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news5}>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 6</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news6}>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 7</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news7}>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 8</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news8}>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 9</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
-            <div className={styles.news9}>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
                 <h2>Lorem ipsum 10</h2>
-                <p>asdfasfsdafsdfsafaas</p>
             </div>
         </div>
     );
