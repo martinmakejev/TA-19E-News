ab689951196949ffc140774d27f557ce615c6caf
AndreasLepsuk <60178041+AndreasLepsuk@users.noreply.github.com>
2022-03-09 14:19:25 +0200

    Tõmbasin Cat.jpg pageidesse ning sain pildid newsboxes.js tööle

--- pages/newsboxes.js
+++ pages/newsboxes.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styles from '../styles/boxes.module.css'
-import { Image } from "antd";
-import "antd/dist/antd.css";
+import Image from "next/image";
 
 //logo header
 //nav content
@@ -10,48 +9,92 @@ import "antd/dist/antd.css";
 function newsboxes() {
     return (
         <div className={styles.container}>
-            <div className={styles.mainNews}>'
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+            <div className={styles.mainNews}>
+                <Image 
+                    width={600}
+                    height={600}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum</h2>
             </div>
             <div className={styles.news}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={150}
+                    height={150}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 1</h2>
             </div>
             <div className={styles.news1}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={150}
+                    height={150}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem Ipsum 2</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image 
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 3</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 4</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 5</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 6</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 7</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 8</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 9</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <Image
+                    width={250}
+                    height={250}
+                    src="/../public/Cat.jpg"
+                />
                 <h2>Lorem ipsum 10</h2>
             </div>
         </div>
