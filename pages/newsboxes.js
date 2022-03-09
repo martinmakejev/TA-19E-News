a0c03672d2551b03d4427b897b3bfffd4595568f
AndreasLepsuk <60178041+AndreasLepsuk@users.noreply.github.com>
2022-03-09 13:32:47 +0200

    Changes in newsboxes.js and boxes.module.css

--- pages/newsboxes.js
+++ pages/newsboxes.js
@@ -7,54 +7,55 @@ import "antd/dist/antd.css";
 //nav content
 //footer
 
-export default function newsboxes() {
+function newsboxes() {
     return (
         <div className={styles.container}>
             <div className={styles.mainNews}>'
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum</h2>
             </div>
             <div className={styles.news}>
-                <img width={200} src="../pages/placeholder.svg"/>
-               <h2>Lorem ipsum 1</h2>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
+                <h2>Lorem ipsum 1</h2>
             </div>
             <div className={styles.news1}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem Ipsum 2</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 3</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 4</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 5</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 6</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 7</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 8</h2>
             </div>
             <div className={styles.news2}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 9</h2>
             </div>
             <div className={styles.news3}>
-                <img width={200} src="../pages/placeholder.svg"/>
+                <img width={200} height={200} src="pages/Cat.jpg"/>
                 <h2>Lorem ipsum 10</h2>
             </div>
         </div>
     );
 }
 
+export default newsboxes;
