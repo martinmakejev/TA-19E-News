6ace6f3294cfacd8f41e50609dc804659020c7c1
AndreasLepsuk <60178041+AndreasLepsuk@users.noreply.github.com>
2022-03-09 10:29:56 +0200

    Kolib ümber

--- pages/newsboxes.js
+++ pages/newsboxes.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from '../styles/boxes.module.css'
+import { Image } from "antd";
+import "antd/dist/antd.css";
 
 //logo header
 //nav content
@@ -8,12 +10,50 @@ import styles from '../styles/boxes.module.css'
 export default function newsboxes() {
     return (
         <div className={styles.container}>
-                <div className={styles.mainNews}>
-                    <h2>Lorem ipsum</h2>
-                    <p>asadfdshfjsdajfhksdfjksdhfuisadjfsda</p>
-                </div>
-                <div className={styles.news}>Lorem ipsum 1</div>
-                <div className={styles.news1}>Lorem ipsum 2</div>
+            <div className={styles.mainNews}>'
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum</h2>
+            </div>
+            <div className={styles.news}>
+                <img width={200} src="../pages/placeholder.svg"/>
+               <h2>Lorem ipsum 1</h2>
+            </div>
+            <div className={styles.news1}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem Ipsum 2</h2>
+            </div>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 3</h2>
+            </div>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 4</h2>
+            </div>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 5</h2>
+            </div>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 6</h2>
+            </div>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 7</h2>
+            </div>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 8</h2>
+            </div>
+            <div className={styles.news2}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 9</h2>
+            </div>
+            <div className={styles.news3}>
+                <img width={200} src="../pages/placeholder.svg"/>
+                <h2>Lorem ipsum 10</h2>
+            </div>
         </div>
     );
 }
