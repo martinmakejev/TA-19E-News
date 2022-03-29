3ee48c713d4495e2dcef91c71e037a10e431fb3a
martinmakejev <martin.makejev@tptlive.ee>
2022-03-29 11:45:52 +0300

    Tegin API kindla uudise pärimiseks läbi ID

--- pages/api/v1/news/[id].js
+++ pages/api/v1/news/[id].js
@@ -2,11 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const handler = async (req, res) => {
   if (req.method === "GET") {
-    const news = await prisma.news.findUnique({
+    let primsaClient = new PrismaClient();
+    const news = await primsaClient.news.findUnique({
       where: {
-        id: req.query.id, // see query.id on siis urli pealt see /api/v1/news/20 => 20
+        id: parseInt(req.query.id), // see query.id on siis urli pealt see /api/v1/news/20 => 20
       },
     });
+    res.status(200).json({ success: true, news });
   }
 };
 export default handler;
