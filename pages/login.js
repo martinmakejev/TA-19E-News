dca91ebaeda56316183290ae918ec49f151662cc
martinmakejev <martin.makejev@tptlive.ee>
2022-04-09 02:44:03 +0300

    updated login

--- pages/login.js
+++ pages/login.js
@@ -1,123 +1,86 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import swal from 'sweetalert';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { Form, Input, Button, Alert, Card } from 'antd';
+import { signIn } from 'next-auth/react';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundSize: 'cover',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+export default function Login() {
+  const [loginError, setLoginError] = useState('');
 
-async function loginUser(credentials) {
-  return fetch('https://tptusers.vercel.app/api/v1/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(credentials),
-  }).then((data) => data.json());
-}
-
-export default function Signin() {
-  const classes = useStyles();
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await loginUser({
-      username,
-      password,
-    });
-    if ('accessToken' in response) {
-      swal('Success', response.message, 'success', {
-        buttons: false,
-        timer: 2000,
-      }).then((value) => {
-        localStorage.setItem('accessToken', response['accessToken']);
-        localStorage.setItem('user', JSON.stringify(response['user']));
-        window.location.href = '/profile';
-      });
-    } else {
-      swal('Failed', response.message, 'error');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const errorParam = urlParams.get('error');
+    if (errorParam === 'CredentialsSignin') {
+      setLoginError('Invalid username or password');
     }
+  }, []);
+
+  const sendCredentials = async (credentials) => {
+    console.log('credentials', credentials);
+    const res = await signIn('credentials', {
+      email: credentials.email,
+      password: credentials.password,
+      callbackUrl: '/',
+      redirect: true,
+    });
+    if (res?.error) setLoginError('Login failed: ' + res.error);
   };
 
   return (
-    <Grid container className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} md={7} className={classes.image} />
-      <Grid item xs={12} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              name="email"
-              label="Email Address"
-              onChange={(e) => setUserName(e.target.value)}
+    <div className="center">
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Card title="Login" bordered={true}>
+        <Form
+          name="basic"
+          onFinish={sendCredentials}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            type="email"
+            rules={[{ required: true, message: 'Input email' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            type="password"
+            style={{ 'margin-bottom': '4px' }}
+            rules={[{ required: true, message: 'Input password' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          {loginError && (
+            <Alert
+              style={{ marginBottom: 24, alignSelf: 'stretch' }}
+              message={loginError}
+              type="error"
+              showIcon
+              closable
             />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
+          )}
+
+          <a>Forgot your password?</a>
+
+          <Form.Item style={{ 'margin-bottom': '4px' }}>
+            <Button type="primary" htmlType="submit">
+              Login
             </Button>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
+          </Form.Item>
+
+          <div>
+            <p style={{ display: 'inline-block', color: '#72767D' }}>
+              Need an account?<a href="/register"> Register</a>
+            </p>
+          </div>
+        </Form>
+      </Card>
+    </div>
   );
 }
