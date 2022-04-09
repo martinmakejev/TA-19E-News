d43b5b9a6b3353b7fb67b6c1fd30583a9054d9ed
Ingela <59916738+JoinEliza@users.noreply.github.com>
2022-04-10 00:20:19 +0300

    change grid to ant grid

--- resources/components/newsboxes.js
+++ resources/components/newsboxes.js
@@ -16,7 +16,7 @@ function Newsboxes() {
     if (error) return "An error has occurred.";
     if (!data) return "Loading...";
     return (
-        <div>
+        <div className={styles.container}>
             <Row>
                 <Col span={12}>
                     <div className={styles.mainNews}>
@@ -31,29 +31,33 @@ function Newsboxes() {
                     </div>
                 </Col>
                 <Col span={12}>
-                    <Row span={12}>
-                        <div className={styles.news}>
-                            <Image
-                                width={150}
-                                height={150}
-                                src={data.latestnews[1].news_images}
-                            />
-                            <h2>{data.latestnews[1].news_title}</h2>
-                            <p>{new Date(data.latestnews[1].release_date).toLocaleDateString()}</p>
-                            <a href={"/posts/"+data.latestnews[1].id}>loe edasi</a>
-                        </div>
+                    <Row>
+                        <Col span={24}>
+                            <div className={styles.news}>
+                                <Image
+                                    width={150}
+                                    height={150}
+                                    src={data.latestnews[1].news_images}
+                                />
+                                <h2>{data.latestnews[1].news_title}</h2>
+                                <p>{new Date(data.latestnews[1].release_date).toLocaleDateString()}</p>
+                                <a href={"/posts/"+data.latestnews[1].id}>loe edasi</a>
+                            </div>
+                        </Col>
                     </Row>
-                    <Row span={12}>
-                        <div className={styles.news1}>
-                            <Image
-                                width={150}
-                                height={150}
-                                src={data.latestnews[2].news_images}
-                            />
-                            <h2>{data.latestnews[2].news_title}</h2>
-                            <p>{new Date(data.latestnews[2].release_date).toLocaleDateString()}</p>
-                            <a href={"/posts/"+data.latestnews[2].id}>loe edasi</a>
-                        </div>
+                    <Row>
+                        <Col span={24}>
+                            <div className={styles.news1}>
+                                <Image
+                                    width={150}
+                                    height={150}
+                                    src={data.latestnews[2].news_images}
+                                />
+                                <h2>{data.latestnews[2].news_title}</h2>
+                                <p>{new Date(data.latestnews[2].release_date).toLocaleDateString()}</p>
+                                <a href={"/posts/"+data.latestnews[2].id}>loe edasi</a>
+                            </div>
+                        </Col>
                     </Row>
                 </Col>
             </Row>
@@ -71,7 +75,7 @@ function Newsboxes() {
                     </div>
                 </Col>
                 <Col span={12}>
-                    <div className={styles.news3}>
+                    <div className={styles.news2}>
                         <Image
                             width={250}
                             height={250}
@@ -101,63 +105,63 @@ function Newsboxes() {
                         <Image
                             width={250}
                             height={250}
-                            src={data.latestnews[5].news_images}
+                            src={data.latestnews[6].news_images}
                         />
-                        <h2>{data.latestnews[5].news_title}</h2>
-                        <p>{new Date(data.latestnews[5].release_date).toLocaleDateString()}</p>
-                        <a href={"/posts/"+data.latestnews[5].id}>loe edasi</a>
+                        <h2>{data.latestnews[6].news_title}</h2>
+                        <p>{new Date(data.latestnews[6].release_date).toLocaleDateString()}</p>
+                        <a href={"/posts/"+data.latestnews[6].id}>loe edasi</a>
                     </div>
                 </Col>
             </Row>
             <Row>
-                <Col span={8}>
+                <Col span={12}>
                     <div className={styles.news2}>
                         <Image
                             width={250}
                             height={250}
-                            src={data.latestnews[5].news_images}
+                            src={data.latestnews[7].news_images}
                         />
-                        <h2>{data.latestnews[5].news_title}</h2>
-                        <p>{new Date(data.latestnews[5].release_date).toLocaleDateString()}</p>
-                        <a href={"/posts/"+data.latestnews[5].id}>loe edasi</a>
+                        <h2>{data.latestnews[7].news_title}</h2>
+                        <p>{new Date(data.latestnews[7].release_date).toLocaleDateString()}</p>
+                        <a href={"/posts/"+data.latestnews[7].id}>loe edasi</a>
                     </div>
                 </Col>
-                <Col span={8}>
+                <Col span={12}>
                     <div className={styles.news2}>
                         <Image
                             width={250}
                             height={250}
-                            src={data.latestnews[5].news_images}
+                            src={data.latestnews[8].news_images}
                         />
-                        <h2>{data.latestnews[5].news_title}</h2>
-                        <p>{new Date(data.latestnews[5].release_date).toLocaleDateString()}</p>
-                        <a href={"/posts/"+data.latestnews[5].id}>loe edasi</a>
+                        <h2>{data.latestnews[8].news_title}</h2>
+                        <p>{new Date(data.latestnews[8].release_date).toLocaleDateString()}</p>
+                        <a href={"/posts/"+data.latestnews[8].id}>loe edasi</a>
                     </div>
                 </Col>
             </Row>
             <Row>
-                <Col span={6}>
+                <Col span={12}>
                     <div className={styles.news2}>
                         <Image
                             width={250}
                             height={250}
-                            src={data.latestnews[5].news_images}
+                            src={data.latestnews[9].news_images}
                         />
-                        <h2>{data.latestnews[5].news_title}</h2>
-                        <p>{new Date(data.latestnews[5].release_date).toLocaleDateString()}</p>
-                        <a href={"/posts/"+data.latestnews[5].id}>loe edasi</a>
+                        <h2>{data.latestnews[9].news_title}</h2>
+                        <p>{new Date(data.latestnews[9].release_date).toLocaleDateString()}</p>
+                        <a href={"/posts/"+data.latestnews[9].id}>loe edasi</a>
                     </div>
                 </Col>
-                <Col span={6}>
+                <Col span={12}>
                     <div className={styles.news2}>
                         <Image
                             width={250}
                             height={250}
-                            src={data.latestnews[5].news_images}
+                            src={data.latestnews[10].news_images}
                         />
-                        <h2>{data.latestnews[5].news_title}</h2>
-                        <p>{new Date(data.latestnews[5].release_date).toLocaleDateString()}</p>
-                        <a href={"/posts/"+data.latestnews[5].id}>loe edasi</a>
+                        <h2>{data.latestnews[10].news_title}</h2>
+                        <p>{new Date(data.latestnews[10].release_date).toLocaleDateString()}</p>
+                        <a href={"/posts/"+data.latestnews[10].id}>loe edasi</a>
                     </div>
                 </Col>
             </Row>
