ccf2e4e0f8dcb7b23eb672e2f6c940dc9e1d861b
Ingela <59916738+ingeliza@users.noreply.github.com>
2022-04-12 11:17:09 +0300

    make header title clickable
    When user clicks on header title, the page redirects on the main page

--- resources/components/header.js
+++ resources/components/header.js
@@ -11,7 +11,7 @@ export default function MainLayout() {
       <Header style={{ backgroundColor: 'white' }}>
         <PageHeader
           ghost={false}
-          title="News"
+          title={<a href="/" style={{color: "black"}}>News</a>}
           extra={[
             <Text>Not signed in</Text>,
             <Button
