80c29f7b9a54425924c49ff5f921902645ecc525
Ingela <59916738+ingeliza@users.noreply.github.com>
2022-04-12 11:21:34 +0300

    Update header.js

--- resources/components/header.js
+++ resources/components/header.js
@@ -1,43 +1,45 @@
 import { Layout, Button, Input, PageHeader, Typography } from 'antd';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const { Header } = Layout;
 const { Search } = Input;
 
-export default function MainLayout() {
-  const { Text } = Typography;
+export default function MainLayout(){
+    const { data: session } = useSession()
+    const { Text } = Typography;
 
-  return (
-    <div>
-      <Header style={{ backgroundColor: 'white' }}>
-        <PageHeader
-          ghost={false}
-          title={<a href="/" style={{color: "black"}}>News</a>}
-          extra={[
-            <Text>Not signed in</Text>,
-            <Button
-              onClick={() => {
-                window.location.href = '/login';
-              }}
-            >
-              Logi sisse
-            </Button>,
-            <Search placeholder="Otsi" style={{ width: 200 }} />,
-          ]}
-        ></PageHeader>
-      </Header>
-    </div>
-  );
-}
-// export default function Component() {
-//   const { data: session } = useSession()
-//   if(session) {
-//     return <>
-//       Signed in as {session.user.email} <br/>
-//       <button onClick={() => signOut()}>Sign out</button>
-//     </>
-//   }
-//   return <>
-//     Not signed in <br/>
-//     <button onClick={() => signIn()}>Sign in</button>
-//   </>
-// }
+
+    if(session) {
+    return(
+        <div>
+            <Header style={{ backgroundColor: 'white'}}>
+                <PageHeader
+                    ghost={false}
+                    title={<a href="/" style={{color: "black"}}>News</a>}
+                    //subTitle=""
+                    extra={[
+                        <Text>Signed in as {session.user.email}</Text>,
+                        <Button onClick={() => signOut()}>Sign out</Button>,
+                        <Search placeholder="Otsi" style={{ width: 200 }} />
+                    ]}
+                ></PageHeader>
+            </Header>
+        </div>
+    )}
+    return(
+        <div>
+            <Header style={{ backgroundColor: 'white'}}>
+                <PageHeader
+                    ghost={false}
+                    title={<a href="/" style={{color: "black"}}>News</a>}
+                    extra={[
+                        <Text>Not signed in</Text>,
+                        <Button onClick={() => signIn()}>Sign in</Button>,
+                        <Search placeholder="Otsi" style={{ width: 200 }} />
+                    ]}
+                ></PageHeader>
+            </Header>
+        </div>
+    )
+
+} 
