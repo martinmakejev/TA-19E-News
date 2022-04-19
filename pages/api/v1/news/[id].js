aa91ab52ef4c1c8e79716e5675d5d4e8cc6d6e02
Artur Allmann <Artur.Allmann@tptlive.ee>
2022-04-20 00:26:19 +0300

    commented out delete, added delete to [id].js

--- pages/api/v1/news/[id].js
+++ pages/api/v1/news/[id].js
@@ -9,6 +9,14 @@ const handler = async (req, res) => {
       },
     });
     res.status(200).json({ success: true, news });
+  } else if (req.method === "DELETE") {
+    let prismaClient = new PrismaClient();
+    const news = await prismaClient.news.delete({
+      where: {
+        id: parseInt(req.query.id), // see query.id on siis urli pealt see /api/v1/news/20 => 20
+      },
+    });
+    res.status(200).json({ success: true, news });
   }
 };
 export default handler;
--- pages/api/v1/news/delete.js
+++ pages/api/v1/news/delete.js
@@ -1,22 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-let prismaClient = new PrismaClient();
+// let prismaClient = new PrismaClient();
 
-export default async function createNews(req, res) {
-  if (req.method === 'DELETE'){
-    try {
-        let news = await prismaClient.news.delete({
-        where:{
-            id: 5
-        },
-        });
-        res.status(200).json({ success: true, news });
-      }catch(err) {
-        res.status(err).json({});}
-      } else {
-        res.status(405);
-        res.end();
-        console.log('failure')
-      }
-    }
-  
+// export default async function createNews(req, res) {
+//   if (req.method === 'DELETE'){
+//     try {
+//         let news = await prismaClient.news.delete({
+//         where:{
+//             id: 5
+//         },
+//         });
+//         res.status(200).json({ success: true, news });
+//       }catch(err) {
+//         res.status(err).json({});}
+//       } else {
+//         res.status(405);
+//         res.end();
+//         console.log('failure')
+//       }
+//     }
